e95abf5d0f5c52dcf4b701f1e5405669f7105237
galindezak <42718365+afrodriguezgal@users.noreply.github.com>
2023-02-02 03:09:19 +0000

    cambios en conexión base de datos y generar report

--- paginas/crearReportePDF.php
+++ paginas/crearReportePDF.php
@@ -9,7 +9,7 @@ function Header()
     // Logo
     //$this->Image('logo.png',10,8,33);
     // Arial bold 15
-    $this->SetFont('Arial','B',15);
+    $this->SetFont('Arial','B',13);
     // Movernos a la derecha
     $this->Cell(80);
     // Título
@@ -17,10 +17,10 @@ function Header()
     // Salto de línea
     $this->Ln(20);
 
-    $this->Cell(50, 10, 'Nombre producto', 1, 0, 'C', 0,);
-    $this->Cell(40, 10, 'Marca', 1, 0, 'C', 0,);
-    $this->Cell(40, 10, 'Precio', 1, 0, 'C', 0,);
-    $this->Cell(40, 10, 'Cantidad', 1, 0, 'C', 0,);
+    $this->Cell(45, 10, 'Nombre producto', 1, 0, 'C', 0,);
+    $this->Cell(45, 10, 'Marca', 1, 0, 'C', 0,);
+    $this->Cell(45, 10, 'Precio', 1, 0, 'C', 0,);
+    $this->Cell(45, 10, 'Cantidad', 1, 1, 'C', 0,);
 }
 
 // Pie de página
@@ -35,7 +35,7 @@ function Footer()
 }
 }
 
-require '../config/db_connection.php';
+require('../config/bdreporte.php');
 $consulta = "SELECT * FROM tabla05";
 $resultado = $mysqli->query($consulta);
 
@@ -45,11 +45,11 @@ $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
 
 while($row = $resultado->fetch_assoc()){
-$pdf->Cell(100, 10, $row['nomProd'], 1, 0, 'C', 0,);
-$pdf->Cell(45, 10, $row['marcProd'], 1, 0, 'C', 0,);
-$pdf->Cell(45, 10, $row['precProd'], 1, 0, 'C', 0,);
-$pdf->Cell(45, 10, $row['cantProd'], 1, 1, 'C', 0,);
+$pdf->Cell(45, 10, $row['nomProd'], 1, 0, 'C', 0);
+$pdf->Cell(45, 10, $row['marcProd'], 1, 0, 'C', 0);
+$pdf->Cell(45, 10, $row['precProd'], 1, 0, 'C', 0);
+$pdf->Cell(45, 10, $row['cantProd'], 1, 1, 'C', 0);
 }
 
 $pdf->Output();
-?>
+?>bdreporte.php
